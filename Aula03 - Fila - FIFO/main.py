6c4dc7da5b8dbc1a78b6b72c693b1eef50956aae
ADALTO SELAU SPARREMBERGER <00661854086@senacrs.edu.br>
2026-04-01 11:39:51 -0300

    Aula03 - Fila - FIFO

---
+++ Aula03 - Fila - FIFO/main.py
@@ -0,0 +1,21 @@
+from Fila import Fila
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.add( "Gisele" )
+fila.add( "Benjamin" )
+fila.add( "Inês" )
+fila.remover()
+fila.add( "Júlia" )
+fila.remover()
+fila.remover()
+fila.remover()
+
+#Exercício para 08/04/2026
+#Implemente uma fila de carros de um lava jato
+# Cada carro deve conter modelo, placa e ano
+# implemente um método para adicionar carro na fila, 
+# um méto para lavar um carro e um método para imprimir 
+# a fila de carros
